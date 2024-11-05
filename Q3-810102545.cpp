97e1848b8ad191b0c8cd6e8ebc0fabcdea006429
FarbodKhodadadi <farbod.khodadadi83@gmail.com>
2024-11-05 23:05:37 +0330

    Initail commit Q3

---
+++ Q3-810102545.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+
+
+int main(){
+
+    int numSchools=0;
+    vector<int> numStudents;
+    vector<int> shirini;
+    cin >> numSchools;
+    
+    numStudents = HandleInput();
+
+}
