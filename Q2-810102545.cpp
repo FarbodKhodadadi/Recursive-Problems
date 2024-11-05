ef3f189713b1505a360a86278c66ba5de244fd50
FarbodKhodadadi <farbod.khodadadi83@gmail.com>
2024-11-05 22:10:28 +0330

    Initial commit for Q2

---
+++ Q2-810102545.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+const char PLUS = '+';
+const char MINUS = '-';
+const char MULTIPLY = '*';
+
+
+
+
+
+
+int main(){
+
+    int n=0;
+    cin >> n ;
+    int numOfTokens = 2*n-1;
+
+    string equation;
+    cin >> equation;
+
+}
